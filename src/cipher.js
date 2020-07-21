e9ad69c88724de2212cf0ce2c788f4728dc4be85
katylurdht <katyhuamanitito1@gmail.com>
2020-07-20 22:07:07 -0500

    agregando codigo de avance 1

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,53 @@
 const cipher = {
+  encode:(offset, string)=>{
+    console.log("*offset "+offset);
+    console.log("*string "+string);
+
+    let msjencode="";
+
+//recomendable variables que solo se usen en este for
+  for(let i=0;i<string.length;i++){
+    let letra_nueva;//¿seria bueno dentroo o fuera por la memoria???
+    let posicion_inicial;
+    if(string[i]!=" "){// si un estring es distinto a un vacio
+      posicion_inicial=parseInt(string.charCodeAt(i));
+      if(posicion_inicial+parseInt(offset)>90){
+        let reinicio_posicion=posicion_inicial+parseInt(offset)-90;
+        console.log("posicion "+ posicion_inicial+parseInt(offset));
+        letra_nueva=String.fromCharCode(64+reinicio_posicion);
+        msjencode+=letra_nueva;// alamacena la nueva letra
+      }
+      else{
+        letra_nueva=String.fromCharCode(posicion_inicial+parseInt(offset));
+        msjencode+=letra_nueva;
+      }
+    }
+    else{
+      msjencode+=" ";
+    }
+  }
+  console.log("mensaje final "+msjencode);
+  return msjencode;
+  },
+
+  decode:(offset, string)=>{
+ console.log("entre");
+    let msjdecode="";
+
+    for(let i=0;i<string.length;i++){
+      console.log("string "+"["+i+"]"+string[i]);
+      let letra_nuevad;
+      let posicion_iniciald;
+      posicion_iniciald=parseInt(string.charCodeAt(i));
+      letra_nuevad=String.fromCharCode(posicion_iniciald-parseInt(offset));
+      msjdecode+=letra_nuevad;// alamacena la nueva letra
+
+    }
+    return msjdecode;
+  }
+
+
+//"1":2
   // ...
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const btn_codificar = document.getElementById("btn_codificar");
+btn_codificar.addEventListener("click", function() {
+let textocdf=  document.getElementById("input_msj").value.toUpperCase();
+let n_posicion= parseInt(document.getElementById("offset").value);
+document.getElementById("mostrando_msj").value= cipher.encode(n_posicion,textocdf);
+});
+
+
+const btn_decodificar = document.getElementById("btn_decodificar");
+btn_decodificar.addEventListener("click", function() {
+let textocdf=  document.getElementById("input_msj").value.toUpperCase();
+let n_posicion= parseInt(document.getElementById("offset").value);
+
+document.getElementById("mostrando_msj").value= cipher.decode(n_posicion,textocdf);
+});
+
+const input_mensaje = document.getElementById("input_msj");
+input_mensaje.addEventListener("keypress", function(event) {
+//function permitir_letras(e) {
+    let tecla = (document.all) ? event.keyCode : event.which;
+
+    //Tecla de retroceso para borrar, siempre la permite
+    if (tecla == 8) {
+        console.log("ingrese ifff");
+
+        return true;
+    }
+console.log("ingrese :ddddd");
+    // Patron de entrada, en este caso solo acepta numeros y letras
+    //patron = /[A-Za-z0-9]/;
+    const patron = /[[^A-Z]/;
+    let tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+});
